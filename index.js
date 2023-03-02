50beacd99151c65d569d66cc41919bf1e9abadab
ign13101 <nacho-13101@hotmail.com>
2023-03-02 18:39:50 +0100

    Google Cloud deployment added

---
+++ index.js
@@ -0,0 +1,78 @@
+
+let associationData = [
+    [
+        44044, 
+        `PADRES Y MADRES DE ALUMNOS EL CASTELLON DEL C.P. RURAL FILABRES`, `A) ASISTIR A LOS PADRES O TUTORES EN TODO AQUELLO QUE CONCIERNE A LA EDUCACION DE SUS HIJOS O REPRESENTADOS. B) COLABORAR EN LAS ACTIVIDADES EDUCATIVAS DE LOS CENTROS, Y EN LAS ACTIVIDADES COMPLEMENTARIAS Y EXTRAESCOLARES DE LOS MISMOS. - C) PROMOVER LA PARTICIPACION DE LOS PADRES DE LOS ALUMNOS EN LA GESTION DEL CENTRO. D) FACILITAR LA REPRESENTACION Y PARTICIPACION DE LOS PADRES DE ALUMNOS EN LOS CONSEJOS ESCOLARES DE LOS CENTROS PUBLICOS Y CONCERTADOS, PARA LO CUAL PODRAN PRESENTAR... - CANDIDATURAS EN LOS CORRESPONDIENTES PROCESOS ELECTORALES. E) PROMOVER EL DESARROLLO DE PROGRAMAS DE EDUCACION FAMILIAR. F) REPRESENTAR A LOS PADRES ASOCIADOS A LAS MISMAS ANTE LAS INSTITUCIONES EDUCATIVAS Y OTROS ORGANISMOS. - G) REPRESENTAR A LOS PADRES ASOCIADOS ANTE LAS INSTANCIAS EDUCATIVAS Y OTROS ORGANISMOS, ADEMAS DE PARTICIPAR EN LOS ORGANOS DEL CENTRO, MUNICIPIO, PROVINCIA, COMUNIDAD AUTONOMA, O ADMINISTRACION CENTRAL DEL ESTADO EN LOS QUE SE HAYA ESTABLECIDO O SE ... - REQUIERA LA PRESENCIA DE LA ASOCIACION. H) ORIENTAR Y ESTIMULAR A LOS PADRES RESPECTO DE LAS OBLIGACIONES QUE LES INCUMBEN EN RELACION CON LA EDUCACION DE SUS HIJOS. I) COLABORAR EN LA MEDIDA QUE ASI LE SEA SOLICITADO, EN LA ELABORACION DESARROLLO O ... - MODIFICAICON DEL REGALMENTO DE ORGANIZACION Y FUNCIONAMIENTO DEL CENTRO. J) FOMENTAR LAS RELACIONES DE COOPERACION DEL CENTRO CON OTROS ESTABLECIMIENTOS ESCOLARES Y CON LOS SECTORES SOCIALES Y CULTURALES DEL ENTORNO. J) PROMOVER LA PLENA REALIZACION DEL.. - PRINCIPIO DE GRATUIDAD EN EL AMBITO DEL CENTRO, COMO LA EFECTIVA IGUALDAD DE DERECHOS DE TODOS LOS ALUMNOS, ASI DISCRIMINACION POR RAZONES DE SEXO, DE CONFORMIDAD CON O DISPUESTO POR EL ART. 27.3 CE Y POR LOS ART. 18.1 Y 20.1 LODE. K) OTROS...`, 
+        2000, 1999, `PLAZA DE ESPAÑA, 1`, 4857, 4, `ALMERIA`, `Albánchez`, 46, 950122001
+    ],
+    [
+        40140, `SOCIOCULTURAL DE INMIGRANTES PALOMA DEL SUR`, `DEFENDER LOS INTERESES MATERIALES Y MORALES DE LOS INMIGRANTES.- TRABAJAR POR LA INTEGRACION DEL INMIGRANTE EN LA SOCIEDAD DE ACOGIDA.- PROMOVER LA RECUPERACION HUMANA COMO PREVIO PLANTEAMIENTO A LA INTEGRACION.- INTERCAMBIO DE LAS DIFERENTES CULTURAS CON LA CULTURA ESPAÑOLA, ANDALUZA.- OFRECER SERVICIOS DE INFORMACION, ORIENTACION Y PROMOCION.- ORGANIZAR, PROMOVER Y PARTICIPAR EN ACTIVIDADES ORMATIVAS, CULTURALES Y DE OCIO.- COLABORAR CON OTROS COLECTIVOS QUE TENGAN LOS MISMOS FINES QUE LA ASOCIACION SOCIOCULTURAL DE EMIGRANTES PALOMA DEL SUR.- FAVORECER LA CONCIENCIA DE LA PROPIA DIGNIDAD Y AUTOESTIMA.- COLABORAR CON DIFERENTES PLATAFORMAS SOCIALES LOCALES, MUNICIPALES Y PROVINCIALES PARA LA CONSECUCION DE LOS FINES QUE SE EXPRESAN EN LOS ESTATUTOS.- CONSOLIDARSE COMO GRUPO REFERENTE DE PARTICIPACION Y PERTENENCIA, DE CARA A GENERAR MAYOR COHERENCIA Y PROTAGONISMO ENTRE EL COLECTIVO INMIGRANTE.- ORGANIZAR ACTIVIDADES DE ENCUENTRO Y CONVIVENCIA INTERCULTURAL CON OTROS INMIGRANTES Y CON LA SOCIEDAD DE ACOGIDA.`, 
+        2000, 1999, `C/ PIO BAROJA, 44`, 4700, 4, `ALMERIA`, `Ejido (El)`, 9026, 686663343
+    ],
+    [
+        57849, `PADRES Y MADRES DEL ALUMNADO SOL DE PONIENTE DEL COLEGIO PUBLICO CEIP PONIENTE DEL SOLANILLO`, `ASISITIR A LOS PADRES, MADRES O TUTORES EN TODO AQUELLO QUE CONCIERNE A LA EDUCACIÓN DE SUS HIJOS O REPRESENTADOS. - COLABORAR EN LAS ACTIVIDADES EDUCATIVAS DEL CENTRO, Y EN LAS ACTIVIDADES COMPLEMENTARIAS Y EXTRAESCOLARES DE LOS MISMOS. - PROMOVER LA PARTICIPACIÓN DE LOS PADRES, MADRES DEL ALUMNADO EN LA GESTIÓN DEL CENTRO. - FACILITAR LA REPRESENTACIÓN Y PARTICIPACIÓN DE LOS PADRES Y MADRES DEL ALUMNADO EN EL CONSEJO ESCOLAR DEL CENTRO, PARA LO CUAL PODRÁN PRESENTAR CANDIDATURAS EN LOS CORRESPONDIENTES PROCESOS ELECTORALES. - PROMOVER EL DESARROLLO DE PROGRAMAS DE EDUCACION FAMILIAR. - REPRESENTAR A LOS PADRES Y MADRES ASOCIADOS ANTE LAS INSTANCIAS EDUCATIVAS Y OTROS ORGANISMOS, ADEMÁS DE PARTICIPAR EN LOS ÓRGANOS DEL CENTRO, MUNICIPIO, PROVINCIA, COMUNIDAD AUTÓNOMA O ADMINISTRACIÓN CENTRAL DE ESTADO EN LOS QUE SE HAYA ESTABLECIDO O ... - ...REQUIERA LA PRESENCIA DE LA ASOCIACION. OTROS.`, 
+        2000, 1999, `PLAZA BARCELONA, 5 -EL SOLANILLO-`, 4740, 4, `ALMERIA`, `Roquetas de Mar`, 792, 950156971
+    ],
+    [
+        41743, `MINUSVALIDOS DE CUEVAS DEL ALMANZORA A.M.C.A.L.`, `DEFENDER LA DIGNIDAD Y LOS DERECHOS DE LAS PERSONAS CON DISCAPACIDAD - PROMOVER LA INTEGRACION ESCOLAR, LABORAL Y SOCIAL DE LAS PERSONAS MINUSVÁLIDAS - PROMOVER ACTITUDES POSITIVAS HACIA LAS PERSONAS MINUSVALIDAS - PROMOVER LA LEGISLACION ADECUADA QUE MEJORE LA SITUACION DEL COLECTIVO DE PERSONAS CON DISCAPACIDAD - SER CAUCE DE COLABORACION ENTRE ORGANISMOS PUBLICOS Y PRIVADOS. - ELEVAR PROPUESTAS SUGERENCIAS Y CONSULTAS A LOS ORGANISMOS RELACIONADOS CON LA PROBLEMATICA SOCIAL DE ESTE COLECTIVO A NIVEL LOCA, REGIONAL O NACIONAL, SIEMPRE QUE SE ESTIME CONVENIENTE. - CUALQUIER OTRO QUE DE MODO DIRECTO O INDIRECTO, PUEDA CONTRIBUIR ALK CUMPLIMIENTO DE LA MISIÓN DE LA ASOCIACION O REDUNDAR EN BENEFICIO DE LAS PERSONAS CON DISCAPACIDAD.`, 
+        2000, 1999, `CALLE LAS LISAS, 21`, 4610, 4, `ALMERIA`, `Cuevas del Almanzora`, 353, 676717697
+    ],
+    [
+        40142, `VIVALDI`, `A) atender a las necesidades deportivas de los vecinos del municipio. - B) promocionar la cultura y el deporte en todas sus extensiones y manifestaciones a traves de la realizacion de talleres de musica, baile y actividades deportivas como escuelas de deporte. (futbol, voleibol, atletismo, ajedrez). - C) recuperacion de tradiciones y costumbres. - D) crear distintas secciones: juventud, mujer o otro tipo que se estime conveniente. - - -`, 
+        2000, 1999, `AVDA. ANDALUCIA Nº 1`, 4692, 4, `ALMERIA`, `Taberno`, 890, 950123040
+    ],
+    [
+        58240, `ALUMNAS Y ALUMNOS DE EDUCACIÓN PERMANENTE BOLLULLOS DEL C.E.P. LA PIÑA", "Colaborar con la comunidad educativa del Centro de Educación Permanete en la educación y en la formación para lograr una formación integral y una educación para la igualdad.La defensa de los derechos del alumnado, en el sentido de que reciban de una forma - eficiente la debida enseñanza del centro de educación permanente " La Piña".Estimular la creación y desarrollo de instituciones, servicios y actividades del Centro de Educación Permanente "La Piña", culturales, pedagógicas, éticas, educación en valores, - educación para la paz y la no violencia, educación en igualdad de oportunidades entre mujeres y hombres y en definitiva a la formación integral del alumnado.Ayudar al Centro de Educación Permanente....... - - - -`, 
+        2007, 2007, `c/ Medina Sidonia, nº 35`, 21710, 21, `HUELVA`, `Bollullos Par del Condado`, 139, 959410243
+    ],
+    [
+        19653, `PEÑA CULTURAL FLAMENCA DE ARJONA`, `a) Estudio, conservación promoción y divulgación del Arte Flamenco en cualquiera de sus formas de expresión, cante, toque y baile, procurando su introducción en los Centros escolares, como elemento cultural y actividad propia del pueblo andaluz. b) Velar por la pureza del Flamenco y luchar contra su adulteración o manipulación en cualquier tipo. c) La promoción de nuevos interpretes del flamenco, naturales o residentes en esta localidad, con recopilación, de ser posible de los antes autóctonos conocidos. d) Promocionar a sus miembros medios materiales, (discografía, bibliografía, conferencias, recitales, tertulias, etc.) que les permitan profundizar en el conocimiento de este arte y les sirva para su recreo. c) Ofrecer a sus miembros y familiares, en todo momento un ambiente de grata y cordial convivencia.`, 
+        1985, 1985, `SAN NICASIO, 4, BAJO A`, 23760, 23, `JAEN`, `Arjona`, 63, 605135114
+    ],
+    [
+        12392, `JOVEN ORQUESTA PROVINCIAL DE MÁLAGA`, 
+        `1º.- El sostenimiento y promoción de su orquesta.- 2º.- Fomentr la afición a la música en la sociedad, difundiendo desde la Orquestal a la de Cámara en la Provncia de Málaga en particular, y en toda Andalucía, en general, con especial atención a los niños y jóvenes.Prestando interés en el ocio y la Educación Inclusiva, así como en la Diversidad Educativa. Desdenuestra Asocición y siempre que haya demanda colaboraremos por integrar en nuestras orquesta personas en necesidades educativas diversas. Dado que la juventud y la educación es nuestra herramienta de trabajo, colaboraremos en los aspectos anteriores.- 3º.- Completar la formación de los niños y jóvenes estudiantes de música, ofreciéndoles una formación musical integral que les permita iniciarse, prepararse o especializarse en la práctica colectiva de la música.- 4º.- La Asociación Joven Orquesta Provincial de Málaga asume como principios fundamentales en su régimen de orden interno y funcionamiento, la igualdad de género entre sus asociados, así como la paridad en sus órganos de gestión y dirección. La Junta Directiva habrá de estar integrada en una proporción de 50% entre asociados y asociadas o viceversa.- 5º.- Así mismo adopta un compromiso claro con el Medio Ambiente, la Ecología y la sostenibilidad. Favoreciendo el reciclaje y el consumo responsable. Las comunicaciones a y entre los componenetes de la Asociación, a partir de ahora pasan a ser vía electrónica. El Transporte de la orquesta a los espacios escénicos donde desarrolle sus actividades, se hará en transporte colectivo.- 6º.- Nuestra asociación, apuesta por las nuevas tecnologías y la innovación, para ello apuesta por el trabajo en red, sindo las comunicaciones, notificaciones, envío de material, certificaciones, audiovisuales y cualquier otra actividad que surja, entre los componentes de la asociación y su entorno, se hará vía telemática y a través de las Redes Sociales.- 7º.- Apostamos por el entendimiento intercultural y la colaboración transfronteriza. Constituye una vocación y objetivo fundamental de nuestra asociación, así como la formación y la integración de la diversidad en nustro común proyecto. Nuestra colaboración transcultural y transfronteriza se basa en los intercambios que cada año las y los jóvenes componentes de la orquesta participan en festivales de orquestas de jóvenes interactuando con las mas variopintas nacionalidades.- 8º.- Resolución de conflictos, mediacion. Para la solución de los conflictos o divergencias que pudieren generarse en el devenir de nuestra asociación las y los componentes de nuestra asociación, se someten de manera expresa a la mediación y arbitraje de la Comisión de asociados que se creará al efecto, y que estará integrada por tres miembros de la asociación elegidos en Asamblesa General con facultades decisorias.- 9º (incluido según acuerdo de la A.G. de 2 La labor de nuestra asociación se está haciendo muy amplia y pensamos que debemos colaborar de forma activa en el tejido de........`, 
+        1994, 1994, `C/ Sèptimo Miau, nº 3, Locales 1 y 2`, 29006, 29, `MALAGA`, `Málaga`, 672, 605985951
+    ],
+    [
+        44925, `CASA DE LA PAZ`, `2.1-CREAR EN SEVILLA UN ESPACIO "CASA DE LA PAZ"QUE SERÁ SEDE DE LOS TRES GRUPOS PROMOTORES,SIEMPRE QUE ESTOS PERTENEZCAN A LA FEDERACION,DONDE REALIZAR LAS ACTIVIDADES PROPIAS DE ESTOS GRUPOS Y LOS DE LA FEDERACION.2.2-SERVIR DE UNION,COORDINACION,INTERC - AMBIO Y ORIENTACION ENTRE LAS ASOCIACIONES FEDERADAS,PARA EL MEJOR DESARROLLO DE SUS FINES COMUNES. - SE ADJUNTA FOTOCOPIA DE FINES. - - - -`, 
+        1979, 1979, `C/ ANICETO SAENZ,Nº 1, LOCAL 3`, 41003, 41, `SEVILLA`, `Sevilla`, 917, 954373313
+    ],
+    [
+        46163, `EMPRESARIAL DE ARTESANOS CASTALLA`, `A) REPRESENTAR A SUS ASOCIADOS EN LA DEFENSA DE SUS INTERESES LABORALES, PROFESIONALES Y ECONOMICOS. B) PROMOCIÓN, CONTROL Y DEFENSA DE LA ACTIVIDAD PROFESIONAL EN EL SECTOR.`, 2000, 2000, `C/ Príncipe de Asturias, nº 42 "Hacienda Santa Bárbara"`, 41950, 41, `SEVILLA`, `Castilleja de la Cuesta`, 299, 637504388
+    ]
+] 
+
+function getTownshipCodeAverage(dataArray) {
+
+        let filteredArray = dataArray.filter(dataRow => dataRow[8] === "ALMERIA");
+        
+        let length = filteredArray.length;
+        let sum = 0;
+        filteredArray.forEach(element => {
+            sum += element[10];
+        });
+
+        /*let sum = filteredArray.reduce((acum, current) => {
+            acum + current[10], initial;
+        })*/
+    return sum/length;
+}
+
+
+var cool = require("cool-ascii-faces");
+var express = require("express");
+
+var app = express();
+var port = process.env.PORT || 12345;
+
+
+app.get("/samples/IGR", (req, res) => {
+    res.send(`` + getTownshipCodeAverage(associationData));
+    console.log(`New request`);
+});
+
+app.listen(port, () => {
+    console.log(`Server ready in port ${port}`);
+});
+
