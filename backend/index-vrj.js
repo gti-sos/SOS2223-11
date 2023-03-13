99fd2b0384a107421ebe67220b29933f5052925c
vicruijim <ruiz.victor.0493@gmail.com>
2023-03-13 14:20:09 +0100

    cambios

--- backend/index-vrj.js
+++ backend/index-vrj.js
@@ -1,6 +1,6 @@
-module.exports = (app)=>{
+module.exports = (app) => {
 
-    var phoneArray= [{
+    var phoneArray = [{
         year: 2021,
         province: "Almeria",
         landline: 222492,
@@ -61,7 +61,7 @@ module.exports = (app)=>{
         province: "Almeria",
         landline: 227071,
         post_payment_phone_line: 510534,
-        wide_landline:171232,
+        wide_landline: 171232,
     },
     {
         year: 2017,
@@ -72,138 +72,147 @@ module.exports = (app)=>{
     }
     ];
 
-BASE_API_URL_PHONE = "/api/v1/phone-line-stats";
-var apiPhoneData = [];
-app.get(BASE_API_URL_PHONE+"/loadInitialData",(req,res)=>{
-    if (apiPhoneData.length === 0){
-        console.log("datos cargados")
-        apiPhoneData = phoneArray;
-       
-    }
-    //res.json(apiPhoneData);
-    res.sendStatus(200);
-});
+    BASE_API_URL_PHONE = "/api/v1/phone-line-stats";
+    var apiPhoneData = [];
+    app.get(BASE_API_URL_PHONE + "/loadInitialData", (req, res) => {
+        if (apiPhoneData.length === 0) {
+            console.log("datos cargados")
+            apiPhoneData = phoneArray;
 
-app.get(BASE_API_URL_PHONE,(req,res)=>{
-    var filters = req.query;
-    var filteredPhones = apiPhoneData.filter(phone => {
-        let isValid = true;
-        for (key in filters) {
-          console.log(key, phone[key], filters[key]);
-          isValid = isValid && phone[key] == filters[key];
         }
-        return isValid;
-      });
-      if (filteredPhones.length!=0){
-            res.json(filteredPhones);
-      }else{
-        res.json(apiPhoneData);
-      }
-    res.sendStatus(200);
+        //res.json(apiPhoneData);
+        res.sendStatus(200);
+    });
 
-});
-app.get(BASE_API_URL_PHONE+"/:province",(req,res)=>{
-    var provincia = req.params.province;
-    var filtro = apiPhoneData.filter(phone=>phone.province === provincia);
-    if (filtro.length!=0){
-        res.json(filtro);
-    }
-    else {
-
-        res.sendStatus(404);
-    }
-    
-});
-
-app.get(BASE_API_URL_PHONE+"/:province/:year",(req,res)=>{
-    var provincia = req.params.province;
-    var year = parseInt(req.params.year);
-    var filtro = apiPhoneData.filter(phone=>phone.province === provincia && phone.year === year);
-    if (filtro.length!=0){
-        res.json(filtro);
-    }
-    else {
-
-        res.sendStatus(404);
-    }
-    
-});
-
-
-app.post(BASE_API_URL_PHONE, (req, res) => {
-    var newPhoneLine = req.body;
-    var provincia = newPhoneLine.province;
-    console.log(provincia);
-    var year = parseInt(newPhoneLine.year);
-    console.log(year);
-    console.log("New POST request to /phone-line-stats");
-    if (apiPhoneData.filter(phone => phone.province === provincia && year === phone.year).length != 0) {
-        console.log("Resource already exists");
-        res.sendStatus(409);
-    }
-    else {
-        apiPhoneData.push(newPhoneLine);
-        res.sendStatus(201);
-    }
-});
-app.post(BASE_API_URL_PHONE+"/:province/:year", (req, res)=>{
-    console.log("Method not allowed");
-    res.sendStatus(405);
-});
-
-app.put(BASE_API_URL_PHONE+"/:province/:year",(req,res)=>{
-    var province = req.params.province;
-    var year = parseInt(req.params.year);
-    var landline = req.body.landline;
-    var post_payment_phone_line = req.body.landline;
-    var wide_landline =  req.body.landline;
-    console.log(`New PUT request to /association-stats/${province}/${year}`);
-    if (landline && wide_landline && post_payment_phone_line){
-        if (province === req.body.province && parseInt(req.body.year)===year){
-
-        
-        apiPhoneData.map(phone=>{
-            if (phone.province===province && phone.year === year){
-            phone.landline = landline;
-            phone.post_payment_phone_line = post_payment_phone_line;
-            phone.wide_landline = wide_landline;
-            return phone;
-        }else{
-            return phone;
-        }
+    app.get(BASE_API_URL_PHONE, (req, res) => {
+        var filters = req.query;
+        var filteredPhones = apiPhoneData.filter(phone => {
+            let isValid = true;
+            for (key in filters) {
+                console.log(key, phone[key], filters[key]);
+                isValid = isValid && phone[key] == filters[key];
+            }
+            return isValid;
         });
+        if (filteredPhones.length != 0) {
+            res.json(filteredPhones);
+        } else {
+            res.json(apiPhoneData);
+        }
         res.sendStatus(200);
-    }else{
-        res.sendStatus(400);
-    }
-        
-    }else{
-        res.sendStatus(400);
-    }
-});
 
-app.put(BASE_API_URL_PHONE,(req,res)=>{
-    console.log("Method not allowed");
-    res.sendStatus(405);
-});
+    });
+    app.get(BASE_API_URL_PHONE + "/:province", (req, res) => {
+        var provincia = req.params.province;
+        var filtro = apiPhoneData.filter(phone => phone.province === provincia);
+        if (filtro.length != 0) {
+            res.json(filtro);
+        }
+        else {
 
-app.delete(BASE_API_URL_PHONE,(req,res)=>{
-    console.log("New delete to /phone-line-stats");
-    apiPhoneData = [];
-    res.sendStatus(200);
-});
+            res.sendStatus(404);
+        }
 
-app.delete(BASE_API_URL_PHONE+"/:province/:year",(req,res)=>{
-    var province = req.params.province;
-    var year = parseInt(req.params.year);
-    console.log(`New DELETE request to /association-stats/${province}/${year}`);
-    if(apiPhoneData.filter(phone=> phone.province === province && phone.year===year).length>0){
-        borra = apiPhoneData.filter(phone=> phone.province === province && phone.year===year)[0];
-        apiPhoneData = apiPhoneData.filter(phone => phone!=borra);
+    });
+
+    app.get(BASE_API_URL_PHONE + "/:province/:year", (req, res) => {
+        var provincia = req.params.province;
+        var year = parseInt(req.params.year);
+        var filtro = apiPhoneData.filter(phone => phone.province === provincia && phone.year === year);
+        if (filtro.length != 0) {
+            res.json(filtro[0]);
+        }
+        else {
+
+            res.sendStatus(404);
+        }
+
+    });
+
+
+    app.post(BASE_API_URL_PHONE, (req, res) => {
+        var newPhoneLine = req.body;
+        var provincia = newPhoneLine.province;
+        console.log(provincia);
+        var year = parseInt(newPhoneLine.year);
+        console.log(year);
+        var landline = req.body.landline;
+        var post_payment_phone_line = req.body.post_payment_phone_line;
+        var wide_landline = req.body.wide_landline;
+        console.log("New POST request to /phone-line-stats");
+        if (landline && post_payment_phone_line && wide_landline && Object.values(req.body).length === 5) {
+            apiPhoneData.push(newPhoneLine);
+            res.sendStatus(201);
+            
+        } else {
+
+            if (apiPhoneData.filter(phone => phone.province === provincia && year === phone.year).length != 0) {
+                console.log("Resource already exists");
+                res.sendStatus(409);
+            }
+            else {
+                res.sendStatus(400);
+            }
+        }
+    });
+    app.post(BASE_API_URL_PHONE + "/:province/:year", (req, res) => {
+        console.log("Method not allowed");
+        res.sendStatus(405);
+    });
+
+    app.put(BASE_API_URL_PHONE + "/:province/:year", (req, res) => {
+        var province = req.params.province;
+        var year = parseInt(req.params.year);
+        var landline = req.body.landline;
+        var post_payment_phone_line = req.body.landline;
+        var wide_landline = req.body.landline;
+        console.log(`New PUT request to /association-stats/${province}/${year}`);
+        if (landline && wide_landline && post_payment_phone_line) {
+            if (province === req.body.province && parseInt(req.body.year) === year) {
+
+
+                apiPhoneData.map(phone => {
+                    if (phone.province === province && phone.year === year) {
+                        phone.landline = landline;
+                        phone.post_payment_phone_line = post_payment_phone_line;
+                        phone.wide_landline = wide_landline;
+                        return phone;
+                    } else {
+                        return phone;
+                    }
+                });
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(400);
+            }
+
+        } else {
+            res.sendStatus(400);
+        }
+    });
+
+    app.put(BASE_API_URL_PHONE, (req, res) => {
+        console.log("Method not allowed");
+        res.sendStatus(405);
+    });
+
+    app.delete(BASE_API_URL_PHONE, (req, res) => {
+        console.log("New delete to /phone-line-stats");
+        apiPhoneData = [];
         res.sendStatus(200);
-    }else{
-        res.sendStatus(404);
-    }
-});
+    });
+
+    app.delete(BASE_API_URL_PHONE + "/:province/:year", (req, res) => {
+        var province = req.params.province;
+        var year = parseInt(req.params.year);
+        console.log(`New DELETE request to /association-stats/${province}/${year}`);
+        if (apiPhoneData.filter(phone => phone.province === province && phone.year === year).length > 0) {
+            borra = apiPhoneData.filter(phone => phone.province === province && phone.year === year)[0];
+            apiPhoneData = apiPhoneData.filter(phone => phone != borra);
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    });
 
 }
