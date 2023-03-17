72062d373ab93679ab977e2b075e6bef06f2728e
ign13101 <nacho-13101@hotmail.com>
2023-03-17 10:32:17 +0100

    Removed creation_date and address attributes for Ignacio's data

--- backend/index_igr.js
+++ backend/index_igr.js
@@ -9,8 +9,6 @@ module.exports = (app) => {
             name: `PADRES Y MADRES DE ALUMNOS EL CASTELLON DEL C.P. RURAL FILABRES`,
             goal: `A) ASISTIR A LOS PADRES O TUTORES EN TODO AQUELLO QUE CONCIERNE A LA EDUCACION DE SUS HIJOS O REPRESENTADOS. B) COLABORAR EN LAS ACTIVIDADES EDUCATIVAS DE LOS CENTROS, Y EN LAS ACTIVIDADES COMPLEMENTARIAS Y EXTRAESCOLARES DE LOS MISMOS. - C) PROMOVER LA PARTICIPACION DE LOS PADRES DE LOS ALUMNOS EN LA GESTION DEL CENTRO. D) FACILITAR LA REPRESENTACION Y PARTICIPACION DE LOS PADRES DE ALUMNOS EN LOS CONSEJOS ESCOLARES DE LOS CENTROS PUBLICOS Y CONCERTADOS, PARA LO CUAL PODRAN PRESENTAR... - CANDIDATURAS EN LOS CORRESPONDIENTES PROCESOS ELECTORALES. E) PROMOVER EL DESARROLLO DE PROGRAMAS DE EDUCACION FAMILIAR. F) REPRESENTAR A LOS PADRES ASOCIADOS A LAS MISMAS ANTE LAS INSTITUCIONES EDUCATIVAS Y OTROS ORGANISMOS. - G) REPRESENTAR A LOS PADRES ASOCIADOS ANTE LAS INSTANCIAS EDUCATIVAS Y OTROS ORGANISMOS, ADEMAS DE PARTICIPAR EN LOS ORGANOS DEL CENTRO, MUNICIPIO, PROVINCIA, COMUNIDAD AUTONOMA, O ADMINISTRACION CENTRAL DEL ESTADO EN LOS QUE SE HAYA ESTABLECIDO O SE ... - REQUIERA LA PRESENCIA DE LA ASOCIACION. H) ORIENTAR Y ESTIMULAR A LOS PADRES RESPECTO DE LAS OBLIGACIONES QUE LES INCUMBEN EN RELACION CON LA EDUCACION DE SUS HIJOS. I) COLABORAR EN LA MEDIDA QUE ASI LE SEA SOLICITADO, EN LA ELABORACION DESARROLLO O ... - MODIFICAICON DEL REGALMENTO DE ORGANIZACION Y FUNCIONAMIENTO DEL CENTRO. J) FOMENTAR LAS RELACIONES DE COOPERACION DEL CENTRO CON OTROS ESTABLECIMIENTOS ESCOLARES Y CON LOS SECTORES SOCIALES Y CULTURALES DEL ENTORNO. J) PROMOVER LA PLENA REALIZACION DEL.. - PRINCIPIO DE GRATUIDAD EN EL AMBITO DEL CENTRO, COMO LA EFECTIVA IGUALDAD DE DERECHOS DE TODOS LOS ALUMNOS, ASI DISCRIMINACION POR RAZONES DE SEXO, DE CONFORMIDAD CON O DISPUESTO POR EL ART. 27.3 CE Y POR LOS ART. 18.1 Y 20.1 LODE. K) OTROS...`,
             registration_date: 2000,
-            creation_date: 1999,
-            address: `PLAZA DE ESPAÑA, 1`,
             zip_code: 4857,
             province_code: 4,
             province: `ALMERIA`,
@@ -22,8 +20,6 @@ module.exports = (app) => {
             name: `CULTURAL ARTISTICA DUENDE Y COMPAS`,
             goal: `ESTUDIO, REALIZACIÓN Y DIVULGACIÓN DEL FLAMENCO EN SUS VERSIONES DE CANTE, GUITARRA, BAILE Y PERCUSIÓN. - CURSOS DE BAILE, TOQUE Y CANTE. - CONFERENCIAS, VIAJES CULTURALES Y DE CONVIVENCIA. - - - -`,
             registration_date: 2001,
-            creation_date: 2000,
-            address: `CORDONEROS, 6 BAJO`,
             zip_code: 4002,
             province_code: 4,
             province: `ALMERIA`,
@@ -35,8 +31,6 @@ module.exports = (app) => {
             name: `JUVENIL TEATROAK`,
             goal: `DIFUSION CULTURAL. - - - - - -`,
             registration_date: 2002,
-            creation_date: 2002,
-            address: `GRANADA, 80 2º-B`,
             zip_code: 4003,
             province_code: 4,
             province: `ALMERIA`,
@@ -48,8 +42,6 @@ module.exports = (app) => {
             name: `SOCIEDAD CASINO PRINCIPAL`,
             goal: `LA LUCHA, CON INICIATIVAS PROPIAS O EN COLABORACIÓN CON OTRAS ASOCIACIONES, ORGANIZACIONES NO GUBERNAMENTALES O ENTIDADES PÚBLICAS POR LA ERRADICACIÓN EN LA INJUSTICIA SOCIAL. - LA PROMOCIÓN Y DIFUSIÓN DE LA CULTURA, PRESTANDO ESPECIAL ATENCIÓN A LA HISTORIA, GASTRONOMÍA, FOLCLORE Y ARTESANÍA DE NUESTRO ÁMBITO DE ACTUACIÓN. - EL MECENAZGO DE LAS ARTES Y LAS LETRAS, CON ESPECIAL DEDICACIÓN A LOS ESCRITORES Y ARTISTAS RESIDENTES EN NUESTRO ÁMBITO DE ACTUACIÓN. - DOTAR A SUS ASOCIADOS Y FAMILIARES DE UN LUGAR DE REUNIÓN Y ESPARCIMIENTOS. - - -`,
             registration_date: 1966,
-            creation_date: 1966,
-            address: `CURA VALERA, 5`,
             zip_code: 4600,
             province_code: 4,
             province: `ALMERIA`,
@@ -61,8 +53,6 @@ module.exports = (app) => {
             name: `PADRES DE ALUMNOS SIERRA DE GADOR DEL I.E.S. SANTO DOMINGO`,
             goal: `Asistir a los padres o tutores en todo aquello que concierne a la educación de sus hijos. - Colaborar en las actividades educativas de los Centros, y en las actividades complementarias y extraescolares de los mismos. - Promover la participación de los padres de alumnos en la gestión del centro. - Asistir a los padres de alumnos e el jercicio de su derecho a intervenir en el control y gestión del centro. - Facilitar la representacion y participación de los padres de alumnos en el Consejo Escolar del Centro y en cualquiera otro organo colegiado del mismo, y en su caso, municipio o provincia o cualquiera otra instancia pública de ámbito territorial mas amplio - promoviendo candidatos, velando por el cumplimiento y la legalidad del proceso de elección de los mismos y colaborando en la correcta realización de las elecciones. -`,
             registration_date: 1971,
-            creation_date: 1970,
-            address: `AVENIDA OASIS, S/N`,
             zip_code: 4700,
             province_code: 4,
             province: `ALMERIA`,
@@ -74,8 +64,6 @@ module.exports = (app) => {
             name: `ALUMNAS Y ALUMNOS DE EDUCACIÓN PERMANENTE BOLLULLOS DEL C.E.P. LA PIÑA`,
             goal: `Colaborar con la comunidad educativa del Centro de Educación Permanete en la educación y en la formación para lograr una formación integral y una educación para la igualdad.La defensa de los derechos del alumnado, en el sentido de que reciban de una forma - eficiente la debida enseñanza del centro de educación permanente " La Piña".Estimular la creación y desarrollo de instituciones, servicios y actividades del Centro de Educación Permanente "La Piña", culturales, pedagógicas, éticas, educación en valores, - educación para la paz y la no violencia, educación en igualdad de oportunidades entre mujeres y hombres y en definitiva a la formación integral del alumnado.Ayudar al Centro de Educación Permanente....... - - - -`,
             registration_date: 2007,
-            creation_date: 2007,
-            address: `c/ Medina Sidonia, nº 35`,
             zip_code: 21710,
             province_code: 21,
             province: `HUELVA`,
@@ -87,8 +75,6 @@ module.exports = (app) => {
             name: `PEÑA CULTURAL FLAMENCA DE ARJONA`,
             goal: `a) Estudio, conservación promoción y divulgación del Arte Flamenco en cualquiera de sus formas de expresión, cante, toque y baile, procurando su introducción en los Centros escolares, como elemento cultural y actividad propia del pueblo andaluz. b) Velar por la pureza del Flamenco y luchar contra su adulteración o manipulación en cualquier tipo. c) La promoción de nuevos interpretes del flamenco, naturales o residentes en esta localidad, con recopilación, de ser posible de los antes autóctonos conocidos. d) Promocionar a sus miembros medios materiales, (discografía, bibliografía, conferencias, recitales, tertulias, etc.) que les permitan profundizar en el conocimiento de este arte y les sirva para su recreo. c) Ofrecer a sus miembros y familiares, en todo momento un ambiente de grata y cordial convivencia.`,
             registration_date: 1985,
-            creation_date: 1985,
-            address: `SAN NICASIO, 4, BAJO A`,
             zip_code: 23760,
             province_code: 23,
             province: `JAEN`,
@@ -100,8 +86,6 @@ module.exports = (app) => {
             name: `JOVEN ORQUESTA PROVINCIAL DE MÁLAGA`,
             goal: `1º.- El sostenimiento y promoción de su orquesta.- 2º.- Fomentr la afición a la música en la sociedad, difundiendo desde la Orquestal a la de Cámara en la Provncia de Málaga en particular, y en toda Andalucía, en general, con especial atención a los niños y jóvenes.Prestando interés en el ocio y la Educación Inclusiva, así como en la Diversidad Educativa. Desdenuestra Asocición y siempre que haya demanda colaboraremos por integrar en nuestras orquesta personas en necesidades educativas diversas. Dado que la juventud y la educación es nuestra herramienta de trabajo, colaboraremos en los aspectos anteriores.- 3º.- Completar la formación de los niños y jóvenes estudiantes de música, ofreciéndoles una formación musical integral que les permita iniciarse, prepararse o especializarse en la práctica colectiva de la música.- 4º.- La Asociación Joven Orquesta Provincial de Málaga asume como principios fundamentales en su régimen de orden interno y funcionamiento, la igualdad de género entre sus asociados, así como la paridad en sus órganos de gestión y dirección. La Junta Directiva habrá de estar integrada en una proporción de 50% entre asociados y asociadas o viceversa.- 5º.- Así mismo adopta un compromiso claro con el Medio Ambiente, la Ecología y la sostenibilidad. Favoreciendo el reciclaje y el consumo responsable. Las comunicaciones a y entre los componenetes de la Asociación, a partir de ahora pasan a ser vía electrónica. El Transporte de la orquesta a los espacios escénicos donde desarrolle sus actividades, se hará en transporte colectivo.- 6º.- Nuestra asociación, apuesta por las nuevas tecnologías y la innovación, para ello apuesta por el trabajo en red, sindo las comunicaciones, notificaciones, envío de material, certificaciones, audiovisuales y cualquier otra actividad que surja, entre los componentes de la asociación y su entorno, se hará vía telemática y a través de las Redes Sociales.- 7º.- Apostamos por el entendimiento intercultural y la colaboración transfronteriza. Constituye una vocación y objetivo fundamental de nuestra asociación, así como la formación y la integración de la diversidad en nustro común proyecto. Nuestra colaboración transcultural y transfronteriza se basa en los intercambios que cada año las y los jóvenes componentes de la orquesta participan en festivales de orquestas de jóvenes interactuando con las mas variopintas nacionalidades.- 8º.- Resolución de conflictos, mediacion. Para la solución de los conflictos o divergencias que pudieren generarse en el devenir de nuestra asociación las y los componentes de nuestra asociación, se someten de manera expresa a la mediación y arbitraje de la Comisión de asociados que se creará al efecto, y que estará integrada por tres miembros de la asociación elegidos en Asamblesa General con facultades decisorias.- 9º (incluido según acuerdo de la A.G. de 2 La labor de nuestra asociación se está haciendo muy amplia y pensamos que debemos colaborar de forma activa en el tejido de........`,
             registration_date: 1994,
-            creation_date: 1994,
-            address: `C/ Sèptimo Miau, nº 3, Locales 1 y 2`,
             zip_code: 29006,
             province_code: 29,
             province: `MALAGA`,
@@ -113,8 +97,6 @@ module.exports = (app) => {
             name: `CASA DE LA PAZ`,
             goal: `2.1-CREAR EN SEVILLA UN ESPACIO "CASA DE LA PAZ"QUE SERÁ SEDE DE LOS TRES GRUPOS PROMOTORES,SIEMPRE QUE ESTOS PERTENEZCAN A LA FEDERACION,DONDE REALIZAR LAS ACTIVIDADES PROPIAS DE ESTOS GRUPOS Y LOS DE LA FEDERACION.2.2-SERVIR DE UNION,COORDINACION,INTERC - AMBIO Y ORIENTACION ENTRE LAS ASOCIACIONES FEDERADAS,PARA EL MEJOR DESARROLLO DE SUS FINES COMUNES. - SE ADJUNTA FOTOCOPIA DE FINES. - - - -`,
             registration_date: 1979,
-            creation_date: 1979,
-            address: `C/ ANICETO SAENZ,Nº 1, LOCAL 3`,
             zip_code: 41003,
             province_code: 41,
             province: `SEVILLA`,
@@ -126,8 +108,6 @@ module.exports = (app) => {
             name: `EMPRESARIAL DE ARTESANOS CASTALLA`,
             goal: `A) REPRESENTAR A SUS ASOCIADOS EN LA DEFENSA DE SUS INTERESES LABORALES, PROFESIONALES Y ECONOMICOS. B) PROMOCIÓN, CONTROL Y DEFENSA DE LA ACTIVIDAD PROFESIONAL EN EL SECTOR.`,
             registration_date: 2000,
-            creation_date: 2000,
-            address: `C/ Príncipe de Asturias, nº 42 "Hacienda Santa Bárbara"`,
             zip_code: 41950,
             province_code: 41,
             province: `SEVILLA`,
@@ -191,17 +171,15 @@ module.exports = (app) => {
         const nameReq = req.body.name;
         const goalReq = req.body.goal;
         const regReq = req.body.registration_date;
-        const creReq = req.body.creation_date;
-        const addReq = req.body.address;
         const zipReq = req.body.zip_code;
         const proCodeReq = req.body.province_code;
         const proReq = req.body.province;
         const townReq = req.body.township;
         const townCodeReq = req.body.township_code;
-        const isRequestBodyValid = (idReq !== undefined) && (nameReq !== undefined) && (goalReq !== undefined) && (regReq !== undefined) && (creReq !== undefined) && (addReq !== undefined) && (zipReq !== undefined) && (proCodeReq !== undefined) && (proReq !== undefined) && (townReq !== undefined) && (townCodeReq !== undefined);
+        const isRequestBodyValid = (idReq !== undefined) && (nameReq !== undefined) && (goalReq !== undefined) && (regReq !== undefined) && (zipReq !== undefined) && (proCodeReq !== undefined) && (proReq !== undefined) && (townReq !== undefined) && (townCodeReq !== undefined);
         const newAssociation = req.body;
         console.log("New POST request to /association-stats");
-        if (!(isRequestBodyValid) || !(Object.values(req.body).length === 11)) {
+        if (!(isRequestBodyValid) || !(Object.values(req.body).length === 9)) {
             console.log("Body request format not valid");
             res.sendStatus(400);
             return;
@@ -310,8 +288,6 @@ module.exports = (app) => {
         const nameReq = req.body.name;
         const goalReq = req.body.goal;
         const regReq = req.body.registration_date;
-        const creReq = req.body.creation_date;
-        const addReq = req.body.address;
         const zipReq = req.body.zip_code;
         const proCodeReq = req.body.province_code;
         const proReq = req.body.province;
@@ -326,8 +302,6 @@ module.exports = (app) => {
                     id: idReq,
                     name: nameReq,
                     goal: goalReq,
-                    creation_date: creReq,
-                    address: addReq,
                     zip_code: zipReq,
                     province_code: proCodeReq,
                     township: townReq,
