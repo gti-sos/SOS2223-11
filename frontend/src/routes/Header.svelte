bc1f78f552a9fc7abe03eb35096772a9f09087ea
ign13101 <nacho-13101@hotmail.com>
2023-04-10 13:55:09 +0200

    Added spanish

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -18,13 +18,11 @@
 
     <Nav navbar>
       <NavItem>
-        <NavLink href="/" class="text-white">Home</NavLink>
+        <NavLink href="/" class="text-white">Inicio</NavLink>
       </NavItem>
-      <NavItem>
-        <NavLink href="#" class="text-white">About</NavLink>
-      </NavItem>
+      
       <Dropdown nav inNavbar>
-        <DropdownToggle nav caret class="text-white">Frontend</DropdownToggle>
+        <DropdownToggle nav caret class="text-white">Interfaces</DropdownToggle>
         <DropdownMenu end>
           <DropdownItem
             ><NavLink href="/association-stats" class="text-black"
