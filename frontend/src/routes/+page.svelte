cd722b08ddc5741dd94df01376600085300bbe5b
ign13101 <nacho-13101@hotmail.com>
2023-04-10 15:25:13 +0200

    Added spanish translation for home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,152 +5,179 @@
         {
             name: "Víctor Ruiz Jiménez",
             github: "https://github.com/vicruijim",
-            dataname: "phone-line-stats"
+            dataname: "phone-line-stats",
         },
         {
             name: "Ignacio García Rodríguez",
             github: "https://github.com/ign13101",
-            dataname: "association-stats"
+            dataname: "association-stats",
         },
         {
             name: "Christian Asuero Carrellán",
             github: "https://github.com/chrasucar342",
-            dataname: "projection-home-stats"
+            dataname: "projection-home-stats",
         },
     ];
 </script>
 
 <Container>
     <div class="pageContainer">
-        <br>
+        <br />
 
         <h2 id="sos2223-11">SOS2223-11</h2>
         <hr />
-        
-    <Row>
-        <Col>
-            <strong>Team</strong>
-            <ul>
-                {#each teamMembers as member}
-                <li><a href={member.github}>{member.name}:</a> <i>{member.dataname}</i></li>
-                {/each}
-            </ul>
-        </Col>
-    </Row>
 
-    <Row>
-        <Col>
-            <p>
-                <strong>Project description</strong>: Our project aims to
-                analyze the correlations between associations, telephone lines
-                and household structures.
-            </p>
-        </Col>
-    </Row>
+        <Row>
+            <Col>
+                <strong>Team</strong>
+                <ul>
+                    {#each teamMembers as member}
+                        <li>
+                            <a href={member.github}>{member.name}:</a> Fuente de
+                            datos:<i>{member.dataname}</i>
+                        </li>
+                    {/each}
+                </ul>
+            </Col>
+        </Row>
 
-    <Row>
-        <Col>
-            <p>
-                <strong>Repository</strong>:
-                <a href="https://github.com/gti-sos/SOS2223-11"
-                    >gti-sos/SOS2223-11</a
-                >
-            </p>
-        </Col>
-    </Row>
+        <Row>
+            <Col>
+                <p>
+                    <strong>Descripción del proyecto</strong>: Nuestro proyecto
+                    tiene como objetivo analizar las correlaciones entre
+                    asociaciones, líneas de teléfono y estructuras de hogares.
+                </p>
+            </Col>
+        </Row>
 
-    <Row>
-        <Col>
-            <p>
-                <strong>URL</strong>:
-                <a href="https://sos2223-11.ew.r.appspot.com/"
-                    >https://sos2223-11.ew.r.appspot.com/</a
-                >
-            </p>
-        </Col>
-    </Row>
+        <Row>
+            <Col>
+                <p>
+                    <strong>Repositorio</strong>:
+                    <a href="https://github.com/gti-sos/SOS2223-11"
+                        >gti-sos/SOS2223-11</a
+                    >
+                </p>
+            </Col>
+        </Row>
 
-    <Row>
-        <Col>
-            <p><strong>Postman Documentation</strong></p>
-            <ul>
-                <li>
-                    v1
-                    <ul>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v1/phone-line-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v1/phone-line-stats/docs</a
-                            > (developed by Víctor Ruiz Jiménez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v1/association-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v1/association-stats/docs</a
-                            > (developed by Ignacio García Rodríguez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v1/projection-homes-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v1/projection-homes-stats/docs</a
-                            > (developed by Christian Asuero Carrellán)
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    v2
-                    <ul>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats/docs</a
-                            > (developed by Víctor Ruiz Jiménez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/association-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/association-stats/docs</a
-                            > (developed by Ignacio García Rodríguez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats/docs"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats/docs</a
-                            > (developed by Christian Asuero Carrellán)
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </Col>
-    </Row>
-    <Row>
-        <Col>
-            <p><strong>APIs</strong></p>
-                    <ul>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats</a
-                            > (developed by Víctor Ruiz Jiménez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/association-stats"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/association-stats</a
-                            > (developed by Ignacio García Rodríguez)
-                        </li>
-                        <li>
-                            <a
-                                href="https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats"
-                                >https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats</a
-                            > (developed by Christian Asuero Carrellán)
-                        </li>
-                    </ul>
-               
-        </Col>
-    </Row>
-    <br>
-</div>
+        <Row>
+            <Col>
+                <p>
+                    <strong>URL del proyecto</strong>:
+                    <a href="https://sos2223-11.ew.r.appspot.com/"
+                        >https://sos2223-11.ew.r.appspot.com/</a
+                    >
+                </p>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <p><strong>Interfaces</strong></p>
+                <ul>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/phone-line-stats"
+                            >https://sos2223-11.ew.r.appspot.com/phone-line-stats</a
+                        > (desarrollada por Víctor Ruiz Jiménez)
+                    </li>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/association-stats"
+                            >https://sos2223-11.ew.r.appspot.com/association-stats</a
+                        > (desarrollada por Ignacio García Rodríguez)
+                    </li>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/projection-homes-stats"
+                            >https://sos2223-11.ew.r.appspot.com/projection-homes-stats</a
+                        > (desarrollada por Christian Asuero Carrellán)
+                    </li>
+                </ul>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <p><strong>APIs</strong></p>
+                <ul>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats"
+                            >https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats</a
+                        > (desarrollada por Víctor Ruiz Jiménez)
+                    </li>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/api/v2/association-stats"
+                            >https://sos2223-11.ew.r.appspot.com/api/v2/association-stats</a
+                        > (desarrollada por Ignacio García Rodríguez)
+                    </li>
+                    <li>
+                        <a
+                            href="https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats"
+                            >https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats</a
+                        > (desarrollada por Christian Asuero Carrellán)
+                    </li>
+                </ul>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <p><strong>Documentacion de Postman</strong></p>
+                <ul>
+                    <li>
+                        v1
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v1/phone-line-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v1/phone-line-stats/docs</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v1/association-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v1/association-stats/docs</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v1/projection-homes-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v1/projection-homes-stats/docs</a
+                                >
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        v2
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats/docs</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v2/association-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v2/association-stats/docs</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    href="https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats/docs"
+                                    >https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats/docs</a
+                                >
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </Col>
+        </Row>
+
+        <br />
+    </div>
 </Container>
 
 <style>
