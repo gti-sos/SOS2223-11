e79f62a99e9b382d750d0d51f799656bf314f0c8
vicruijim <ruiz.victor.0493@gmail.com>
2023-04-10 08:56:41 +0200

    esto tiene fin?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,14 +5,17 @@
         {
             name: "Víctor Ruiz Jiménez",
             github: "https://github.com/vicruijim",
+            dataname: "phone-line-stats"
         },
         {
             name: "Ignacio García Rodríguez",
             github: "https://github.com/ign13101",
+            dataname: "association-stats"
         },
         {
             name: "Christian Asuero Carrellán",
             github: "https://github.com/chrasucar342",
+            dataname: "projection-home-stats"
         },
     ];
 </script>
@@ -29,7 +32,7 @@
             <strong>Team</strong>
             <ul>
                 {#each teamMembers as member}
-                <li><a href={member.github}>{member.name}</a></li>
+                <li><a href={member.github}>{member.name}:</a> <i>{member.dataname}</i></li>
                 {/each}
             </ul>
         </Col>
@@ -69,7 +72,7 @@
 
     <Row>
         <Col>
-            <p><strong>APIs</strong></p>
+            <p><strong>Postman Documentation</strong></p>
             <ul>
                 <li>
                     v1
@@ -118,6 +121,32 @@
                     </ul>
                 </li>
             </ul>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <p><strong>APIs</strong></p>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats"
+                                >https://sos2223-11.ew.r.appspot.com/api/v2/phone-line-stats</a
+                            > (developed by Víctor Ruiz Jiménez)
+                        </li>
+                        <li>
+                            <a
+                                href="https://sos2223-11.ew.r.appspot.com/api/v2/association-stats"
+                                >https://sos2223-11.ew.r.appspot.com/api/v2/association-stats</a
+                            > (developed by Ignacio García Rodríguez)
+                        </li>
+                        <li>
+                            <a
+                                href="https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats"
+                                >https://sos2223-11.ew.r.appspot.com/api/v2/projection-homes-stats</a
+                            > (developed by Christian Asuero Carrellán)
+                        </li>
+                    </ul>
+               
         </Col>
     </Row>
     <br>
