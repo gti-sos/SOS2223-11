e9006c099957574d3f67ca81526f2c2422c9e629
vicruijim <ruiz.victor.0493@gmail.com>
2023-05-19 17:42:11 +0200

    jajajajajajajajajajajajajajajajajaj

---
+++ frontend/src/routes/phone-line-stats/integrations/pounded-animals/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+     import { onMount } from 'svelte';
+  import Highcharts from 'highcharts';
+
+  let datos
+const url = 'https://data.gov.au/data/api/3/action/datastore_search?resource_id=316d96a5-715b-43ff-b7ba-af1bd02c536d&limit=20';
+    const options = {
+    method: 'GET',
+};
+onMount(async () => {
+    const response = await fetch(url, options);
+    if (response.ok){
+      datos = await response.json();
+      console.log("API response:", datos.result.records);
+     
+    }
+});
+</script>
