6d731cee338a6c4700efbecde09e914d5d627c3b
vicruijim <ruiz.victor.0493@gmail.com>
2023-05-19 16:11:15 +0200

    line highcharts usado

--- frontend/src/routes/phone-line-stats/integrations/integracion/+page.svelte
+++ frontend/src/routes/phone-line-stats/integrations/integracion/+page.svelte
@@ -1,7 +1,6 @@
 <script>
  import { onMount } from 'svelte';
-  import { dev } from "$app/environment";
-  import * as echarts from 'echarts';
+  import Highcharts from 'highcharts';
    
 let datos
 const url = 'https://data.sa.gov.au/data/api/3/action/datastore_search?resource_id=850833e8-2fde-4c33-b17d-19742b722fc3&limit=5';
@@ -11,10 +10,42 @@ const url = 'https://data.sa.gov.au/data/api/3/action/datastore_search?resource_
 onMount(async () => {
     const response = await fetch(url, options);
     if (response.ok){
-        datos = await response.json();
+      datos = await response.json();
       console.log("API response:", datos.result.records);
     }
-let chartOptions ;
+  let anos = datos.result.records.map(e => e.year);
+  console.log(anos);
+  let serie = [
+    {
+      name: "aboriginal housing probationary",
+      data: datos.result.records.map(e=> e["aboriginal housing probationary"])
+    },
+    {
+      name: "public housing probationary",
+      data: datos.result.records.map(e=> e["public housing probationary"])
+    },
+   ]
 
+   Highcharts.chart('chart-container', {
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: 'Evolución de viviendas'
+        },
+        xAxis: {
+          categories: anos
+        },
+        yAxis: {
+          title: {
+            text: 'Cantidad'
+          }
+        },
+        series: serie
+      });
 });
 </script>
+
+<div id="chart-container">
+
+</div>
