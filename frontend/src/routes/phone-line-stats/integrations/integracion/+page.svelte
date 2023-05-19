5a17eab3b3d6d8d3671d2e0088e20e5fbe9c6074
vicruijim <ruiz.victor.0493@gmail.com>
2023-05-19 14:37:06 +0200

    suputamadre

---
+++ frontend/src/routes/phone-line-stats/integrations/integracion/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+ import { onMount } from 'svelte';
+  import { dev } from "$app/environment";
+  import * as echarts from 'echarts';
+   
+let datos
+const url = 'https://data.sa.gov.au/data/api/3/action/datastore_search?resource_id=850833e8-2fde-4c33-b17d-19742b722fc3&limit=5';
+    const options = {
+    method: 'GET',
+};
+onMount(async () => {
+    const response = await fetch(url, options);
+    if (response.ok){
+        datos = await response.json();
+      console.log("API response:", datos.result.records);
+    }
+let chartOptions ;
+
+});
+</script>
