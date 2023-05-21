7274bbbe5c47ad4185126df71db9e4ee499c434f
ign13101 <nacho-13101@hotmail.com>
2023-05-21 15:55:37 +0200

    Testing e2e

--- e2e/associations.spec.js
+++ e2e/associations.spec.js
@@ -28,7 +28,7 @@ test('Verifies the number of associations to be above one statelessly', async ({
   await page.goto('https://sos2223-11.ew.r.appspot.com/association-stats');
 
   await page.locator('button:has-text("Cargar asociaciones"):not(:has-text("Recargar asociaciones"))').click();
-  await page.waitForSelector('tbody tr');
+  // await page.waitForSelector('tbody tr');
   const rows = await page.locator('tbody tr').count();
   expect(rows).toBeGreaterThan(1);
 });
