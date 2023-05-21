01c4720f7366d17e9137359faf62c5e3d484592d
vicruijim <ruiz.victor.0493@gmail.com>
2023-05-21 16:52:23 +0200

    test e2e

---
+++ e2e/phones.spec.js
@@ -0,0 +1,29 @@
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+    await page.goto('http://sos2223-11.appspot.com/phone-line-stats');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('Lineas telefonicas');
+  });
+
+test("Load Data",async ({ page }) => {
+  await page.goto('http://sos2223-11.appspot.com/phone-line-stats');
+  await page.waitForTimeout(2000);
+  await page.locator("#carga").click();
+  await page.waitForTimeout(2000);
+  await expect((await page.locator(".phoneRow").all()).length).toBeGreaterThanOrEqual(10);
+});
+
+
+test("Delete All",async ({ page }) => {
+  await page.goto('http://sos2223-11.appspot.com/phone-line-stats');
+  await page.waitForTimeout(2000);
+  await page.locator("#acciones").click();
+  await page.waitForTimeout(1000);
+  page.locator('#Borrar').click();
+  await page.waitForTimeout(2000);
+  await expect((await page.locator(".phoneRow").all()).length).toEqual(0);
+});
+
