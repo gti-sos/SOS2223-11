a39b01f794b8fea52c7892327a16252a84216b24
ign13101 <nacho-13101@hotmail.com>
2023-05-21 18:24:38 +0200

    Video added

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -25,7 +25,17 @@
     <h1 class="m-3">Vídeos</h1>
     <div class="m-4">
         <h2 class="my-3">Vídeo de Ignacio García Rodríguez</h2>
-        <Button href="/">Vídeo</Button>
+        <div class="m-2">
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/VMMUDoD4tF8"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+        </div>
+        <Button href="https://drive.google.com/file/d/14ocLB89IGhqF4W-UNDTQPMhLQB1Mm5zg/view?usp=share_link">Vídeo en Google Drive</Button>
     </div>
     <div class="m-4">
         <h2 class="my-3">Vídeo de Victor Ruiz Jiménez</h2>
